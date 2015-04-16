9565b9fa63486c15bf3a7407a9d943e2e350c1ba
Brian Bailey <brian@bailey.is>
2015-04-15 17:23:44 -0700

    Tied background color animations to UIStepper

--- Tips/CalcViewController.swift
+++ Tips/CalcViewController.swift
@@ -14,11 +14,29 @@ class CalcViewController: UIViewController {
     @IBOutlet weak var finalLabel: UILabel!
     @IBOutlet weak var splitLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var numberButton: UIButton!
     @IBOutlet var calcView: UIView!
+    @IBOutlet weak var detailViewButton: UIView!
     
+    // keypad outlets
+    @IBOutlet weak var key0: UIButton!
+    @IBOutlet weak var key1: UIButton!
+    @IBOutlet weak var key2: UIButton!
+    @IBOutlet weak var key3: UIButton!
+    @IBOutlet weak var key4: UIButton!
+    @IBOutlet weak var key5: UIButton!
+    @IBOutlet weak var key6: UIButton!
+    @IBOutlet weak var key7: UIButton!
+    @IBOutlet weak var key8: UIButton!
+    @IBOutlet weak var key9: UIButton!
+    @IBOutlet weak var keyDel: UIButton!
+    
+    // stepper outlets
     @IBOutlet weak var tipStepper: UIStepper!
     @IBOutlet weak var splitStepper: UIStepper!
+    
+    var accentColor : UIColor! = UIColor(red: 255/255, green: 255/225, blue: 255/225, alpha: 0.5)
+    var cornerRadius : CGFloat! = 32
+    var keySize : CGSize! = CGSize(width: 65, height: 65)
     
     var isEditing: Bool! = false
     var expression: String! = "um..." // text when calc value is nil
@@ -37,8 +55,72 @@ class CalcViewController: UIViewController {
         splitStepper.value = 1
         
         // Setup keypad borders
+        key0.backgroundColor = UIColor.clearColor()
+        key0.layer.cornerRadius = cornerRadius
+        key0.layer.borderWidth = 1.5
+        key0.layer.borderColor = accentColor.CGColor
+        key0.frame.size = keySize
         
-        // hide controls on load
+        key1.backgroundColor = UIColor.clearColor()
+        key1.layer.cornerRadius = cornerRadius
+        key1.layer.borderWidth = 1.5
+        key1.layer.borderColor = accentColor.CGColor
+        key1.frame.size = keySize
+        
+        key2.backgroundColor = UIColor.clearColor()
+        key2.layer.cornerRadius = cornerRadius
+        key2.layer.borderWidth = 1.5
+        key2.layer.borderColor = accentColor.CGColor
+        key2.frame.size = keySize
+        
+        key3.backgroundColor = UIColor.clearColor()
+        key3.layer.cornerRadius = cornerRadius
+        key3.layer.borderWidth = 1.5
+        key3.layer.borderColor = accentColor.CGColor
+        key3.frame.size = keySize
+        
+        key4.backgroundColor = UIColor.clearColor()
+        key4.layer.cornerRadius = cornerRadius
+        key4.layer.borderWidth = 1.5
+        key4.layer.borderColor = accentColor.CGColor
+        key4.frame.size = keySize
+        
+        key5.backgroundColor = UIColor.clearColor()
+        key5.layer.cornerRadius = cornerRadius
+        key5.layer.borderWidth = 1.5
+        key5.layer.borderColor = accentColor.CGColor
+        key5.frame.size = keySize
+        
+        key6.backgroundColor = UIColor.clearColor()
+        key6.layer.cornerRadius = cornerRadius
+        key6.layer.borderWidth = 1.5
+        key6.layer.borderColor = accentColor.CGColor
+        key6.frame.size = keySize
+        
+        key7.backgroundColor = UIColor.clearColor()
+        key7.layer.cornerRadius = cornerRadius
+        key7.layer.borderWidth = 1.5
+        key7.layer.borderColor = accentColor.CGColor
+        key7.frame.size = keySize
+        
+        key8.backgroundColor = UIColor.clearColor()
+        key8.layer.cornerRadius = cornerRadius
+        key8.layer.borderWidth = 1.5
+        key8.layer.borderColor = accentColor.CGColor
+        key8.frame.size = keySize
+        
+        key9.backgroundColor = UIColor.clearColor()
+        key9.layer.cornerRadius = cornerRadius
+        key9.layer.borderWidth = 1.5
+        key9.layer.borderColor = accentColor.CGColor
+        key9.frame.size = keySize
+        // end keypad setup
+        
+        // make $0 large on initial load
+        self.totalLabel.transform = CGAffineTransformMakeScale(1.5, 1.5)
+        self.totalLabel.frame.origin.y = 75
+        
+        // hide controls on initial load
         self.tipStepper.hidden = true
         self.splitStepper.hidden = true
         self.finalLabel.hidden = true
@@ -51,6 +133,7 @@ class CalcViewController: UIViewController {
         self.tipLabel.alpha = 0
 
         updateLabels()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -128,7 +211,20 @@ class CalcViewController: UIViewController {
             self.splitLabel.hidden = false
             self.tipLabel.hidden = false
             }, completion: { (True) -> Void in
- 
+        })
+    }
+    
+    func scaleUpBill(){
+        UIView.animateWithDuration(animationTiming, animations: { () -> Void in
+            self.totalLabel.transform = CGAffineTransformMakeScale(1.5, 1.5)
+            self.totalLabel.frame.origin.y = 75
+        })
+    }
+    
+    func scaleDownBill(){
+        UIView.animateWithDuration(animationTiming, animations: { () -> Void in
+            self.totalLabel.transform = CGAffineTransformMakeScale(1, 1)
+            self.totalLabel.frame.origin.y = 25
         })
     }
     
@@ -143,6 +239,7 @@ class CalcViewController: UIViewController {
             totalBill = number
             isEditing = true
             fadeInControls()
+            scaleDownBill()
         } else if (totalBill == expression) {
             totalBill = number
         } else { // add number to end of string
@@ -161,6 +258,10 @@ class CalcViewController: UIViewController {
             totalBill = expression
             isEditing = false
             fadeOutControls()
+            scaleUpBill()
+            UIView.animateWithDuration(animationTiming, animations: { () -> Void in
+                self.calcView.backgroundColor = UIColor.blackColor()
+            })
         } else if (totalBill == expression) {
             // nothing
         } else {
@@ -171,10 +272,19 @@ class CalcViewController: UIViewController {
     }
     
     @IBAction func tipStepper(sender: UIStepper) {
-        println("tapped tipStepper")
+        println("\(tipStepperValue)% tip")
         tipStepperValue = sender.value
-        
         updateLabels()
+        
+        if (tipStepperValue >= 18){
+            UIView.animateWithDuration(animationTiming, animations: { () -> Void in
+                self.calcView.backgroundColor = UIColor(red: 15/255, green: 157/255, blue: 88/255, alpha: 1) // green
+            })
+        } else if (tipStepperValue <= 17){
+            UIView.animateWithDuration(animationTiming, animations: { () -> Void in
+                self.calcView.backgroundColor = UIColor(red: 198/255, green: 18/255, blue: 11/255, alpha: 1) // red
+            })
+        }
     }
     
     @IBAction func splitStepper(sender: UIStepper) {
@@ -185,12 +295,22 @@ class CalcViewController: UIViewController {
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func detailViewButtonDidPress(sender: UITapGestureRecognizer) {
+        println("tapped detail view button")
+    }
+    
+    func detailViewAnimation(){
+        UIView.animateWithDuration(animationTiming, animations: { () -> Void in
+            self.animationTiming
+        })
+    }
 
 }
